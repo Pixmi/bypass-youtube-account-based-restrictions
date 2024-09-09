5e5b13dfe79f19eba2af1ebfbbb7656a689cf8b9
Pixmi <admin@pixmi.dev>
2024-09-09 17:03:26 +0800

    first upload

---
+++ README.md
@@ -0,0 +1,5 @@
+## This scripts will not restore your account from a restricted status!
+
+If your YouTube account is restricted and you are unable to watch videos while logged in, you can try using this script. Otherwise, you don't need to install it.
+
+The script works by changing the video to an embed format and re-embedding it. If it doesn't work properly, please try refreshing the page (F5).
---
+++ script.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Bypass YouTube Account-Based Restrictions
+// @namespace    https://greasyfork.org
+// @version      0.0.6
+// @description  This scripts will not restore your account from a restricted status!
+// @author       Pixmi
+// @match        https://*.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @license      GPL-3.0
+// @grant        none
+// @compatible   Chrome
+// @compatible   Firefox
+// ==/UserScript==
