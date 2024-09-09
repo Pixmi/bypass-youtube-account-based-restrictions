32c002a48ab84f96cab56f7ccf953ece804ffa23
Pixmi <admin@pixmi.dev>
2024-09-09 17:16:03 +0800

    update announcement

--- script.user.js
+++ script.user.js
@@ -4,6 +4,10 @@
 // @version      0.0.6
 // @description  This scripts will not restore your account from a restricted status!
 // @author       Pixmi
+// @homepage     https://github.com/Pixmi/bypass-youtube-account-based-restrictions
+// @updateURL    https://github.com/Pixmi/bypass-youtube-account-based-restrictions/raw/main/script.meta.js
+// @downloadURL  https://github.com/Pixmi/bypass-youtube-account-based-restrictions/raw/main/script.user.js
+// @supportURL   https://github.com/Pixmi/bypass-youtube-account-based-restrictions/issues
 // @match        https://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @license      GPL-3.0
